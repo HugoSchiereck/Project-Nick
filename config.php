761ca6508bde05485cf5565f2936d8cf5aaae977
HugoSchiereck <h.schiereck@draads.nl>
2026-06-16 16:18:41 +0200

    Database inloggegevens definitief gemaakt met nieuw wachtwoord

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ session_start();
 $db_host = 'localhost';
 $db_name = 'mstlog_portal';
 $db_user = 'mstlog_dbuser';
-$db_pass = '6z87Su&0d'; // <-- Plak hier exact je nieuwe wachtwoord
+$db_pass = 'a1#u4y38B'; // <-- Plak hier exact je nieuwe wachtwoord
 
 try {
     $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charset=utf8mb4", $db_user, $db_pass);
